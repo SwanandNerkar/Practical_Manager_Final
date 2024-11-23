0886d2dbd21c94d016d7131b5f3e37f8c855a065
SwanandNerkar <143278555+SwanandNerkar@users.noreply.github.com>
2024-11-23 23:48:25 +0530

    index.js

--- api/index.js
+++ api/index.js
@@ -10,6 +10,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;  
 
+app.get("/", (req, res)=>{
+  res.json({
+    success: true,
+    message: "Server Running Successfully"
+  })
+})
 
 app.use(express.json());
 
